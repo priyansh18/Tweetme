09b692695b0cd5d8f2ddb658ae0fc7b7ca4d0f0c
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-02-16 21:15:49 +0530

    follower,following & Toggle-flow

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url
 from django.views.generic.base import RedirectView
-from .views import UserDetailView
+from .views import UserDetailView,UserFollowView
 
 urlpatterns = [
     # url(r'^$',RedirectView.as_view(url ="/")),
     # url(r'^create/$',TweetCreateView.as_view(),name='create'),
     url(r'^(?P<username>[\w.@+-]+)/$', UserDetailView.as_view(),name='det'),
+    url(r'^(?P<username>[\w.@+-]+)/follow/$', UserFollowView.as_view(),name='follow'),
     # url(r'^search/$',TweetListView.as_view(),name='list'),
     # url(r'^(?P<pk>\d+)/update/$', TweetUpdateView.as_view(),name='update'),
     # url(r'^(?P<pk>\d+)/delete/$', TweetDeleteView.as_view(),name='delete'),
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth import get_user_model
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404,redirect
 from django.views.generic import DetailView
-
+from django.views import View
+from .models import UserProfile
 # Create your views here.
 User = get_user_model()
 
@@ -11,3 +12,20 @@ class UserDetailView(DetailView):
 
     def get_object(self):
         return get_object_or_404(User, username__iexact=self.kwargs.get("username"))
+
+
+    def get_context_data(self,*args,**kwargs):
+        context = super(UserDetailView,self).get_context_data(*args,**kwargs)
+        context['following'] = UserProfile.objects.is_following(self.request.user,self.get_object())
+        return context   
+
+
+class UserFollowView(View):
+    def get(self,request,username,*args,**kwargs):
+        toggle_user = get_object_or_404(User, username__iexact=username)
+        if request.user.is_authenticated:
+            is_following = UserProfile.objects.toggle_follow(request.user,toggle_user)
+            print(is_following)
+        return redirect("accounts:det",username=username)    
+        
+
